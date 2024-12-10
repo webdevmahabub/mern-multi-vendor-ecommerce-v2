ac6c7986cb1a7e562997465b2b144dd1d91eda7a
webdevmahabub <mahbubbd926@gmail.com>
2024-12-10 16:39:57 +0600

    Orders Get and Show Part 1

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,11 +2,12 @@ import authReducer from "../store/Reducers/authReducer";
 import categoryReducer from "./Reducers/categoryReducer";
 import productReducer from "./Reducers/productReducer";
 import sellerReducer from "./Reducers/sellerReducer";
-
+import OrderReducer from "./Reducers/OrderReducer";
 const rootReducer = {
     auth: authReducer,
     category: categoryReducer,
     product: productReducer,
-    seller: sellerReducer
+    seller: sellerReducer,
+    order: OrderReducer
 }
 export default rootReducer;
