89349a124d2f3c53aa3dcca108d92e44aab4d3dc
webdevmahabub <mahbubbd926@gmail.com>
2024-12-14 00:11:16 +0600

    Admin Order Details Get and Show

--- src/store/Reducers/OrderReducer.js
+++ src/store/Reducers/OrderReducer.js
@@ -1,13 +1,12 @@
-
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../api/api";  
- 
+
 export const get_admin_orders = createAsyncThunk(
     'orders/get_admin_orders',
     async({ parPage,page,searchValue },{rejectWithValue, fulfillWithValue}) => {
-        
+
         try {
-             
+
             const {data} = await api.get(`/admin/orders?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`,{withCredentials: true})  
             return fulfillWithValue(data)
         } catch (error) { 
@@ -15,8 +14,36 @@ export const get_admin_orders = createAsyncThunk(
         }
     }
 )
+
   // End Method  
- 
+
+  export const get_admin_order = createAsyncThunk(
+    'orders/get_admin_order',
+    async( orderId ,{rejectWithValue, fulfillWithValue}) => { 
+        try { 
+            const {data} = await api.get(`/admin/order/${orderId}`,{withCredentials: true})  
+            return fulfillWithValue(data)
+        } catch (error) { 
+            return rejectWithValue(error.response.data)
+        }
+    }
+) 
+  // End Method  
+
+  export const admin_order_status_update = createAsyncThunk(
+    'orders/admin_order_status_update',
+    async( {orderId,info} ,{rejectWithValue, fulfillWithValue}) => { 
+        try { 
+            const {data} = await api.put(`/admin/order-status/update/${orderId}`,info,{withCredentials: true})  
+            return fulfillWithValue(data)
+        } catch (error) { 
+            return rejectWithValue(error.response.data)
+        }
+    }
+) 
+  // End Method
+
+
 export const OrderReducer = createSlice({
     name: 'order',
     initialState:{
@@ -27,10 +54,12 @@ export const OrderReducer = createSlice({
         myOrders: []
     },
     reducers : {
+
         messageClear : (state,_) => {
             state.errorMessage = ""
             state.successMessage = ""
         }
+
     },
     extraReducers: (builder) => {
         builder
@@ -39,9 +68,18 @@ export const OrderReducer = createSlice({
             state.myOrders = payload.orders;
             state.totalOrder = payload.totalOrder; 
         })
-        
- 
+        .addCase(get_admin_order.fulfilled, (state, { payload }) => {
+            state.order = payload.order; 
+        })
+        .addCase(admin_order_status_update.rejected, (state, { payload }) => {
+            state.errorMessage = payload.message; 
+        })
+        .addCase(admin_order_status_update.fulfilled, (state, { payload }) => {
+            state.successMessage = payload.message; 
+        })
+
     }
+
 })
 export const {messageClear} = OrderReducer.actions
 export default OrderReducer.reducer
